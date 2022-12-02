e84c615dd3c7f570694af820aa35fe28b90c254d
Jordan McDougall <jordanmcdougall@hotmail.co.uk>
2022-12-02 10:34:16 +0000

    fix: update markdown headers

--- 2022/init.js
+++ 2022/init.js
@@ -17,7 +17,7 @@ rl.question('What is the title of this challenge?\n\n', title => {
         
         fs.mkdirSync(folderName, 0744)
         
-        fs.writeFileSync(`${folderName}/README.md`, `# ${title}\n\n## ${new Date().toDateString()}\n\n# Part One\n\n# Part Two\n`)
+        fs.writeFileSync(`${folderName}/README.md`, `# ${title}\n\n## ${new Date().toDateString()}\n\n### Part One\n\n### Part Two\n`)
         fs.writeFileSync(`${folderName}/index.js`, ``)
         fs.writeFileSync(`${folderName}/input.txt`, ``)
         fs.writeFileSync(`${folderName}/input-sample.txt`, ``)
